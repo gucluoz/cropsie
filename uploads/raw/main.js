dd93ae60218cd3946017d17fa6dd78b1a9f7cc2c
Guclu Ozturk <gucluozturk@gmail.com>
2015-12-26 19:16:55 +0200

    Add js and css files

---
+++ uploads/raw/main.js
@@ -0,0 +1,31 @@
+function loadRandomImage(){
+  $.get("/api/v1/raw/", function(d, s){
+    if (d.filename)
+    {
+      $("#crop-container").croppie("bind",{ url: d.filename});
+      $("#rawId").val(d.id);
+    }
+  });
+}
+
+function saveCrop(){
+  $("#crop-container").croppie("result", "canvas", "viewport").then(function (resp) {
+    if (resp){
+      $.post("/api/v1/processed/"+$("#rawId").val(),{"data": resp})
+        .done(loadRandomImage);
+    }
+  });
+}
+
+var Crop = (function(){
+  function init() {
+    $("#crop-container").croppie({
+      viewport: { width: 50, height: 50, type: "square"},
+      boundary: { width: 800, height: 600},
+      mouseWheelZoom: true
+    });
+    loadRandomImage(); 
+  }
+
+  return { init: init };
+})();
---
+++ uploads/raw/upload.js
@@ -0,0 +1,7 @@
+Dropzone.options.uploadForm = { 
+  parallelUploads: 10,
+  maxFilesize: 7,
+  uploadMultiple: false,
+  clickable: true,
+  acceptedFiles: '.jpg, .jpeg'
+};
