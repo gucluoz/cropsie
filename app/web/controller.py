1b6c06f80b83572c45997b14dd2c527667a046ed
Guclu Ozturk <gucluozturk@gmail.com>
2015-12-26 18:02:09 +0200

    Add upload page with dropzone

--- app/web/controller.py
+++ app/web/controller.py
@@ -4,3 +4,7 @@ from flask import render_template
 @web.route('/')
 def index():
   return render_template('index.html.j2')
+
+@web.route('upload/', methods=['GET', 'POST'])
+def upload():
+  return render_template('upload.html.j2')
